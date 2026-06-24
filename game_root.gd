882e70ad9818a018a408e1633a167334704faae2
refkra <la.jernigan88@gmail.com>
2026-06-24 08:36:26 -0700

    Undo service pattern.... for now

--- game_root.gd
+++ game_root.gd
@@ -2,3 +2,42 @@ extends Node
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+## Top-level Game Methods
+
+
+func launch() -> void:
+
+	pass
+
+
+
+func restart() -> void:
+
+	pass
+
+
+
+func start(save_id: StringName) -> void:
+
+	pass
+
+
+
+func end() -> void:
+
+	pass
+
+
+
