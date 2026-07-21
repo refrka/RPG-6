09f6ff15104d25d26db25f65dac02a7a371d23ba
refkra <la.jernigan88@gmail.com>
2026-07-20 18:58:46 -0700

    Character/Object scenes. _initialize()/_load(), some helper functions

--- entity/entity_def.gd
+++ entity/entity_def.gd
@@ -6,3 +6,5 @@ class_name EntityDef extends Resource
 @export var unique_id: StringName
 
 @export var entity_id: StringName
+
+@export var display_name: String
--- entity/entity_node.gd
+++ entity/entity_node.gd
@@ -1,1 +1,78 @@
 class_name EntityNode extends PhysicsBody2D
+
+
+
+
+@export var entity_def: EntityDef
+
+@export var entity_data: EntityData
+
+
+
+
+var initialized:= false
+
+
+
+
+
+
+
+
+
+
+func _initialize() -> bool:
+
+	if initialized:
+
+		return false
+
+	initialized = true
+
+	return true
+
+
+
+
+
+func _load(_entity_data: EntityData = null) -> bool:
+
+	return true
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+func get_entity_def() -> EntityDef:
+
+	return entity_def
+
+
+func get_entity_data() -> EntityData:
+
+	return entity_data
+
+
+func get_display_name() -> String:
+
+	var display_name = "[MissingNo]"
+
+	if entity_def:
+
+		display_name = entity_def.display_name
+
+	return display_name
