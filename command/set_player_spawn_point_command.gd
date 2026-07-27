776ce63bd1e658e360fc6ec36f6ffeaa996f48a8
refkra <la.jernigan88@gmail.com>
2026-07-27 16:49:59 -0700

    Campfire object with new SetPlayerSpawnPointCommand and related event. Gotta chill on the notices

---
+++ command/set_player_spawn_point_command.gd
@@ -0,0 +1,33 @@
+class_name SetPlayerSpawnPointCommand extends Command
+
+
+@export var spawn_id: StringName
+
+
+
+func execute(_data: Dictionary = {}) -> bool:
+
+	if _data.has("spawn_id"):
+
+		spawn_id = _data["spawn_id"]
+
+	var location = Scenes.get_world_scene().get_active_location()
+
+	var spawn_point = location.get_spawn_point(spawn_id)
+
+	var player = Game.get_player()
+
+	player.active_spawn_point = spawn_point
+
+	Events.fire(PlayerSpawnPointSetEvent, {"spawn_point": spawn_point}, true)
+
+	return true
+
+
+
+
+static func run(_data: Dictionary) -> bool:
+
+	var command = SetPlayerSpawnPointCommand.new()
+
+	return command.execute(_data)
---
+++ event/player_spawn_point_set_event.gd
@@ -0,0 +1,9 @@
+class_name PlayerSpawnPointSetEvent extends GameEvent
+
+
+
+
+
+func _get_notice_primary_text() -> String:
+
+	return "Spawn point set"
